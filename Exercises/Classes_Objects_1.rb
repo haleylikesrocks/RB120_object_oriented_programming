838cbed5320218de9f59477a0b8b85de4cda41d5
Haley Rairez <ramirezhaley@gmail.com>
2021-02-19 13:11:10 -0600

    started on the object oriented programing excersices finished the first 3

---
+++ Exercises/Classes_Objects_1.rb
@@ -0,0 +1,16 @@
+# 1 Find the Class
+
+puts "Hello".class
+puts 5.class
+puts [1, 2, 3].class
+
+# 2 Create the Class
+
+class Cat 
+end
+
+# 3 Create the Class
+
+kitty = Cat.new
+
+# 4 What Are You?
